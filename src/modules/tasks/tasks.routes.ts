70c94d5b8d4762ba2dfbb2ada09b653b516ec8d9
Jakub Hypki <37691329+jhypki@users.noreply.github.com>
2025-02-08 23:34:12 +0100

    add RequestHandler casts in routes

--- src/modules/tasks/tasks.routes.ts
+++ src/modules/tasks/tasks.routes.ts
@@ -1,8 +1,8 @@
-import { Router } from 'express';
+import { RequestHandler, Router } from 'express';
 import tasksController from './tasks.controller';
 
 export const tasksRoutes = Router();
 
 // tasksRoutes.get('/:taskId', tasksController.getTaskById);
-tasksRoutes.post('/', tasksController.createTask);
-tasksRoutes.put('/:id/complete', tasksController.completeTask);
+tasksRoutes.post('/', tasksController.createTask as RequestHandler);
+tasksRoutes.put('/:id/complete', tasksController.completeTask as RequestHandler);
